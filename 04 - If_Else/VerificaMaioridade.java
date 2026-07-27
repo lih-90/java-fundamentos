27d1aa40da728944041dde68e37b57c93239793f
Aline Vieira Ometto <aline.ometto90@gmail.com>
2026-07-27 17:08:44 -0300

    Adiciona pasta If_Else

---
+++ 04 - If_Else/VerificaMaioridade.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+
+public class VerificaMaioridade {
+
+public VerificaMaioridade() {
+
+} 
+
+  public static void main(String[] args) {
+  
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Digite a sua idade (em números): ");
+    int idade = sc.nextInt();
+
+    if (idade >= 18)
+      System.out.println("Maior de idade.");
+    else
+      System.out.println("Menor de idade.");
+
+    sc.close();
+
+  }
+}
